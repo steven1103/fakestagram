6610bfe583fba280c163a379ea7519eb54c2ed3e
고태영 <Gotaeyoung@gotaeyeong-ui-MacBook-Air.local>
2020-10-02 14:49:17 +0900

    서버 개발 #2

---
+++ schema.js
@@ -0,0 +1,15 @@
+import path from path
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { makeExecutableSchema } from "apollo-server";
+
+const allTypes = loadFilesSync(path.join(__dirname, "/api/**/*.graphql"))
+const allResolvers = loadFilesSync(path.join(__dirname, "/api/**/*.js"))
+
+const schema = makeExecutableSchema({
+    typeDefs: mergeTypeDefs(allTypes),
+    resolvers: mergeResolvers(allResolvers)
+    });
+
+export default schema
+
